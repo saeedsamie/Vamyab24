f0b957edf4f6ea4e158b2c43e99e79fa0b9e0934
Saeed <saeed.samie.74@gmail.com>
2016-09-03 17:37:20 +0430

    set back color

--- app/src/main/java/com/example/android/vamyab24/SoodYab.java
+++ app/src/main/java/com/example/android/vamyab24/SoodYab.java
@@ -1,13 +1,30 @@
 package com.example.android.vamyab24;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class SoodYab extends AppCompatActivity {
+public class SoodYab extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sood_yab);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+
+            case R.id.SoodYab_go:
+                fadeOutEvrythings();
+
+                break;
+        }
+    }
+    void fadeOutEvrythings(){
+
     }
 }
