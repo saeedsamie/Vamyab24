013e1c63c36ceb0dde378a0a2d0d3cbac8675b96
Saeed <saeed.samie.74@gmail.com>
2016-09-14 15:29:26 +0430

    ui level 1

--- app/src/main/java/com/example/android/vamyab24/MainActivity.java
+++ app/src/main/java/com/example/android/vamyab24/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.vamyab24;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Build;
 import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -13,6 +14,7 @@ import android.transition.TransitionManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.example.android.vamyab24.Back.MyActivityManager;
 
@@ -22,10 +24,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     public MyActivityManager myActivityManager;
 
-    @Override
-    protected void attachBaseContext(Context newBase) {
-        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,12 +34,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //        decorView.setSystemUiVisibility(uiOptions);
         getSupportActionBar().hide();
 
-        ImageButton ib = (ImageButton) findViewById(R.id.imageButton);
-        ib.setOnClickListener(this);
-        ImageButton ib2 = (ImageButton) findViewById(R.id.imageButton2);
-        ib2.setOnClickListener(this);
-//        Button button = (Button) findViewById(R.id.button4);
-//        button.setOnClickListener(this);
+        TextView textView;
+        textView = (TextView) findViewById(R.id.textView);
+        textView.setOnClickListener(this);
+        textView = (TextView) findViewById(R.id.textView2);
+        textView.setOnClickListener(this);
+
+        TextView tv;
+        textView = (TextView)findViewById(R.id.textView);
+        Typeface tf = Typeface.createFromAsset(getAssets(),"fonts/BNazanin+Bold.ttf");
+        textView.setTypeface(tf);
+        textView = (TextView)findViewById(R.id.textView);
+        textView.setTypeface(tf);
 
     }
 
@@ -52,13 +56,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         myActivityManager = new MyActivityManager();
         switch (v.getId()) {
 
-            case R.id.imageButton:
+            case R.id.textView:
                 intent = new Intent(this, myActivityManager.getSoodYabActivity().getClass());
                 startActivity(intent);
                 overridePendingTransition(R.anim.push_down_in, R.anim.push_down_out);
                 finish();
                 break;
-            case R.id.imageButton2:
+            case R.id.textView2:
                 intent = new Intent(this, myActivityManager.getVaamYabActivity().getClass());
                 startActivity(intent);
                 overridePendingTransition(R.anim.push_up_in, R.anim.push_up_out);
