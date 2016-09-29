994bb022333a9b447bf81d785f9b0b975e13c449
sina-a <sina_hunter@hotmail.com>
2016-09-29 11:39:43 +0330

    Hi !

--- app/src/main/java/com/example/android/vamyab24/MainActivity.java
+++ app/src/main/java/com/example/android/vamyab24/MainActivity.java
@@ -15,6 +15,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.view.GestureDetector;
+import android.view.GestureDetector.SimpleOnGestureListener;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
 
 import com.example.android.vamyab24.Back.MyActivityManager;
 
@@ -36,9 +41,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         TextView textView;
         textView = (TextView) findViewById(R.id.textView);
+        textView.setOnTouchListener(new OnSwipeTouchListener(this) {
+            public void onSwipeBottom() {
+                Intent intent;
+                myActivityManager = new MyActivityManager();
+                intent = new Intent(MainActivity.this, myActivityManager.getSoodYabActivity().getClass());
+                startActivity(intent);
+                overridePendingTransition(R.anim.push_down_in, R.anim.push_down_out);
+                finish();
+            }
+
+        });
         textView.setOnClickListener(this);
         textView = (TextView) findViewById(R.id.textView2);
         textView.setOnClickListener(this);
+        textView.setOnTouchListener(new OnSwipeTouchListener(this) {
+            public void onSwipeTop() {
+                Intent intent;
+                myActivityManager = new MyActivityManager();
+                intent = new Intent(MainActivity.this, myActivityManager.getVaamYabActivity().getClass());
+                startActivity(intent);
+                overridePendingTransition(R.anim.push_up_in, R.anim.push_up_out);
+                finish();
+            }
+
+        });
+
+
+
 
         TextView tv;
         textView = (TextView)findViewById(R.id.textView);
@@ -46,6 +76,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         textView.setTypeface(tf);
         textView = (TextView)findViewById(R.id.textView2);
         textView.setTypeface(tf);
+
 
     }
 
@@ -73,8 +104,79 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
+
+
     @Override
     public void onBackPressed() {
         finish();
     }
-}//
+
+    private class OnSwipeTouchListener implements OnTouchListener {
+
+        private final GestureDetector gestureDetector;
+
+        public OnSwipeTouchListener (Context ctx){
+            gestureDetector = new GestureDetector(ctx, new GestureListener());
+        }
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            return gestureDetector.onTouchEvent(event);
+        }
+
+        private final class GestureListener extends SimpleOnGestureListener {
+
+            private static final int SWIPE_THRESHOLD = 100;
+            private static final int SWIPE_VELOCITY_THRESHOLD = 100;
+
+            @Override
+            public boolean onDown(MotionEvent e) {
+                return true;
+            }
+
+            @Override
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                boolean result = false;
+                try {
+                    float diffY = e2.getY() - e1.getY();
+                    float diffX = e2.getX() - e1.getX();
+                    if (Math.abs(diffX) > Math.abs(diffY)) {
+                        if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+                            if (diffX > 0) {
+                                onSwipeRight();
+                            } else {
+                                onSwipeLeft();
+                            }
+                        }
+                        result = true;
+                    }
+                    else if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
+                        if (diffY > 0) {
+                            onSwipeBottom();
+                        } else {
+                            onSwipeTop();
+                        }
+                    }
+                    result = true;
+
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+                }
+                return result;
+            }
+        }
+
+        public void onSwipeRight() {
+        }
+
+        public void onSwipeLeft() {
+        }
+
+        public void onSwipeTop() {
+        }
+
+        public void onSwipeBottom() {
+        }
+    }
+}
+//
